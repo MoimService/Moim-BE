f99494e95b9591b64cd216795afcaf824121edd0
soheeparklee <soheeparklee@gmail.com>
2025-02-11 20:04:07 +0100

    feat: create entity

---
+++ src/main/java/com/codeit/moim/domain/User.java
@@ -0,0 +1,52 @@
+package com.codeit.moim.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private int userId;
+
+    @Column(name= "name", nullable = false)
+    private String name;
+
+    @Column(name= "email", nullable = false)
+    private String email;
+
+    @Column(name= "position", nullable = false)
+    private String position;
+
+    @Column(name= "password", nullable = false)
+    private String password;
+
+    @Column(name= "profile_pic", nullable = false)
+    private String profilePic;
+
+    @Column(name= "intro", nullable = false)
+    private String intro;
+
+    @Column(name= "gender", nullable = false)
+    private String gender;
+
+    @Column(name= "location", nullable = false)
+    private String location;
+
+    @Column(name= "age", nullable = false)
+    private String age;
+
+    @OneToOne(mappedBy= "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private Contact contact;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<UserSkill> userSkillList = new ArrayList<>();
+}
