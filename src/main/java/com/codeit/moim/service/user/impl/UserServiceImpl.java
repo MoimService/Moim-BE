c9a72fa647a64b1dcf6a6007ab627ae9aa2c5999
soheeparklee <soheeparklee@gmail.com>
2025-02-11 23:02:10 +0100

    feat: service login logic

--- src/main/java/com/codeit/moim/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/codeit/moim/service/user/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.codeit.moim.service.user.impl;
 
+import com.codeit.moim.common.config.JwtTokenProvider;
 import com.codeit.moim.common.exception.ApplicationException;
 import com.codeit.moim.common.exception.auth.UserNotFoundException;
 import com.codeit.moim.common.exception.payload.ErrorStatus;
@@ -8,6 +9,10 @@ import com.codeit.moim.repository.UserRepository;
 import com.codeit.moim.service.user.UserService;
 import com.codeit.moim.web.dto.request.auth.LoginRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,15 +21,21 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
     @Override
     public String login(LoginRequest loginRequest) {
 
         String email = loginRequest.email();
         String password = loginRequest.password();
 
-        try{
+        try {
             User user = userRepository.findByEmail(email)
                     .orElseThrow(() -> new UserNotFoundException(email));
+
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return jwtTokenProvider.createToken(email);
 
         }catch(Exception e){
             e.printStackTrace();
@@ -35,6 +46,5 @@ public class UserServiceImpl implements UserService {
             ));
         }
 
-        return null; //token
     }
 }
