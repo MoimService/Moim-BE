966bc00fbf1f9cd24c1b95010430f2f166cf81e2
soheeparklee <soheeparklee@gmail.com>
2025-02-12 23:47:03 +0100

    fix: modify test file to use test.yml

--- src/test/java/com/codeit/moim/MoimApplicationTests.java
+++ src/test/java/com/codeit/moim/MoimApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@ActiveProfiles("test")
 class MoimApplicationTests {
 
     @Test
