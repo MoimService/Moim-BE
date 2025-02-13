d0ad3bacacc79f64952a2bb00f24b1b3313756e3
soheeparklee <soheeparklee@gmail.com>
2025-02-13 13:13:08 +0100

    fix: fix signup endpoint

--- src/main/java/com/codeit/moim/web/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/codeit/moim/web/filter/JwtAuthenticationFilter.java
@@ -71,7 +71,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String[] excludedPaths = {
-                "/api/v1/auths/signup",
+                "/api/v1/auths/signup/**",
                 "/api/v1/auths/login",
                 "/v3/**",
                 "/swagger-ui/**"
