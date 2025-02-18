08e72fddd3b3d1d5495d6637df87a0ab152f26a1
soheeparklee <soheeparklee@gmail.com>
2025-02-18 14:01:17 +0100

    refactor: improve redundancy by using generic

--- src/main/java/com/codeit/moim/service/mymeeting/impl/MyMeetingServiceImpl.java
+++ src/main/java/com/codeit/moim/service/mymeeting/impl/MyMeetingServiceImpl.java
@@ -25,60 +25,60 @@ public class MyMeetingServiceImpl implements MyMeetingService {
     private final MemberRepository memberRepository;
     @Override
     public UpdateMemberStatusResponse updateMemberStatus(int userId, UpdateMemberStatusRequest request) {
+        Meeting meeting = getMeeting(request.meetingId());
         //check if user is manager
-        if(!checkMeetingManager(userId, request.meetingId())) throw new MeetingAccessDeniedException(String.valueOf(request.meetingId()));
-
+        validateMeetingManager(userId, meeting);
         //get member
-        Meeting meeting = meetingRepository.findById(request.meetingId())
-                .orElseThrow(()-> new MeetingNotFoundException(String.valueOf(request.meetingId())));
-        Member member = getMemberWithUserAndMeeting(request.userId(), request.meetingId());
+        Member member = getMemberWithUserAndMeeting(request.userId(), meeting);
         //change member status, save
         MemberStatus status = MemberStatus.valueOf(request.setMemberStatus());
-        if(member.getStatus().equals(MemberStatus.PENDING)){
-            if(status.equals(MemberStatus.APPROVED)){
-                meeting.increaseMemberCount();
-                meetingRepository.save(meeting);
-            }
-            member.updateStatus(status);
-            Member savedMember = memberRepository.save(member);
-            return UpdateMemberStatusResponse.fromEntity(savedMember);
-        }else{
-            throw new MeetingAccessDeniedException(String.valueOf(request.userId()));
+        if(!member.getStatus().equals(MemberStatus.PENDING)) throw new MeetingAccessDeniedException(String.valueOf(request.userId()));
+
+        if(status.equals(MemberStatus.APPROVED)){
+            meeting.increaseMemberCount();
+            meetingRepository.save(meeting);
         }
+        return saveUpdatedMember(member, status);
     }
 
     @Override
     public UpdateMemberToExpelResponse expelMember(int userId, UpdateMemberStatusRequest request) {
-        if(!checkMeetingManager(userId, request.meetingId())) throw new MeetingAccessDeniedException(String.valueOf(request.meetingId()));
-        Meeting meeting = meetingRepository.findById(request.meetingId())
-                .orElseThrow(()-> new MeetingNotFoundException(String.valueOf(request.meetingId())));
-        Member member = getMemberWithUserAndMeeting(request.userId(), request.meetingId());
-        if(member.getStatus().equals(MemberStatus.APPROVED)){
-            member.updateStatus(MemberStatus.EXPEL);
-            Member savedMember = memberRepository.save(member);
-            meeting.decreaseMemberCount();
-            meetingRepository.save(meeting);
-            return new UpdateMemberToExpelResponse(savedMember.getMemberId());
-        }else{
-            throw new MeetingAccessDeniedException(String.valueOf(request.userId()));
-        }
+        Meeting meeting = getMeeting(request.meetingId());
+        //check if user is manager
+        validateMeetingManager(userId, meeting);
+        Member member = getMemberWithUserAndMeeting(request.userId(), meeting);
+        if( !member.getStatus().equals(MemberStatus.APPROVED)) throw new MeetingAccessDeniedException(String.valueOf(request.userId()));
+
+        meeting.decreaseMemberCount();
+        meetingRepository.save(meeting);
+        return saveUpdatedMember(member, MemberStatus.EXPEL);
     }
 
-    private boolean checkMeetingManager(int userId, int meetingId){
-        User managerUser = userRepository.findById(userId)
-                .orElseThrow(()-> new UserNotFoundException(String.valueOf(userId)));
+    private Meeting getMeeting(int meetingId){
         Meeting meeting = meetingRepository.findById(meetingId)
                 .orElseThrow(()-> new MeetingNotFoundException(String.valueOf(meetingId)));
-        if(meeting.getUser().getUserId() == managerUser.getUserId()) return true;
-        else return false;
+        return meeting;
     }
 
-    private Member getMemberWithUserAndMeeting(int userId, int meetingId){
+    private void validateMeetingManager(int userId, Meeting meeting){
+        if(meeting.getUser().getUserId() != userId) throw new MeetingAccessDeniedException(String.valueOf(meeting.getMeetingId()));
+    }
+
+    private Member getMemberWithUserAndMeeting(int userId, Meeting meeting){
         User requestingUser= userRepository.findById(userId)
                 .orElseThrow(()-> new UserNotFoundException(String.valueOf(userId)));
-        Meeting meeting = meetingRepository.findById(meetingId)
-                .orElseThrow(()-> new MeetingNotFoundException(String.valueOf(meetingId)));
         Member member = memberRepository.findByUserAndMeeting(requestingUser, meeting);
         return member;
+    }
+
+    private <T> T saveUpdatedMember(Member member, MemberStatus status){
+        member.updateStatus(status);
+        Member savedMember = memberRepository.save(member);
+
+        if(status == MemberStatus.EXPEL){
+            return (T) new UpdateMemberToExpelResponse(savedMember.getMemberId());
+        }else{
+            return (T) UpdateMemberStatusResponse.fromEntity(savedMember);
+        }
     }
 }
