81e80b6eefa74111c127f5cff1d359b3f9def942
soheeparklee <soheeparklee@gmail.com>
2025-02-19 12:19:36 +0100

    refactor: add swagger description

--- src/main/java/com/codeit/moim/web/controller/LikesController.java
+++ src/main/java/com/codeit/moim/web/controller/LikesController.java
@@ -4,6 +4,9 @@ import com.codeit.moim.repository.CustomUserDetails;
 import com.codeit.moim.service.likes.LikesService;
 import com.codeit.moim.web.dto.response.Response;
 import com.codeit.moim.web.dto.response.likes.CreateLikeResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class LikesController {
     private final LikesService likesService;
 
+    @Operation(
+            summary = "Create likes with userId and meetingId",
+            description = "Not possible when user is manager of meeting. " +
+                    "Not possible when user already liked the meeting"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Update success")
+    })
     @PostMapping
     public Response<CreateLikeResponse> createLikes(
             @AuthenticationPrincipal CustomUserDetails userDetails,
