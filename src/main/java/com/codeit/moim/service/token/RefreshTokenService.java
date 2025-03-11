fb1a15ce2482c02a5a7249c717eed436e480363b
soheeparklee <soheeparklee@gmail.com>
2025-03-12 00:05:07 +0100

    feat: add access token to blacklist when refresh token is expired

--- src/main/java/com/codeit/moim/service/token/RefreshTokenService.java
+++ src/main/java/com/codeit/moim/service/token/RefreshTokenService.java
@@ -3,6 +3,7 @@ package com.codeit.moim.service.token;
 import com.codeit.moim.domain.RefreshToken;
 import com.codeit.moim.web.dto.request.token.TokenRefreshRequest;
 import com.codeit.moim.web.dto.response.token.JwtResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.util.Optional;
@@ -10,11 +11,11 @@ import java.util.Optional;
 public interface RefreshTokenService {
     RefreshToken createRefreshToken(String email);
 
-    RefreshToken verifyExpiration(RefreshToken token);
+    RefreshToken verifyExpiration(RefreshToken token, HttpServletRequest httpServletRequest);
 
     Optional<RefreshToken> findByToken(String requestRefreshToken);
 
-    String refreshToken(TokenRefreshRequest request);
+    String refreshToken(TokenRefreshRequest request, HttpServletRequest httpServletRequest);
     //String refreshToken(String refreshToken);
 
 }
